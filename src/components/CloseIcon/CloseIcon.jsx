58105530723a7e35a79901130c4ad45b14baa4b0
Lawrence Yoshikawa <lyoshikawa3914@gmail.com>
2024-08-31 13:50:34 -0700

    added comments to CloseIcon.jsx

--- src/components/CloseIcon/CloseIcon.jsx
+++ src/components/CloseIcon/CloseIcon.jsx
@@ -2,10 +2,13 @@
 import { React, useState } from 'react';
 import './CloseIcon.css';
 
+/**
+ * This is a component that, once clicked will close the 'InformationWindow' component
+ * @param {Function} - the function  to close the 'InformationWindow'
+ * @returns 
+ */
 export const CloseIcon = ({ onClose }) => {
     
-    
-
     return (
         <div className='CloseContainer' onClick={onClose}>
             
